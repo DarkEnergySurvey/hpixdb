f535ce9999f89b57b10258ba7379e98142de8be4
Matias Carrasco Kind <mgckind@gmail.com>
2016-04-21 14:26:55 -0500

    fixed setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,7 @@ bin_files = glob.glob("bin/*")
 setup(name='hpixDB',
       version ='1.0.1',
       license = "NCSA",
-      description = "Small routine to convert from RA,DEC to HEALPIX pixel indices to
-be ingested in the DB",
+      description = "Small routine to convert from RA,DEC to HEALPIX pixel indices to be ingested in the DB",
       author = "Matias Carrasco Kind",
       author_email = "mcarras2@illinois.edu",
       packages = ['hpixDB'],
