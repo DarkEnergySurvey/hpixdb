4020a1c37dac5e8a4ab405f78ed71e5db1526809
Matias Carrasco Kind <mgckind@gmail.com>
2016-04-21 12:32:53 -0500

    more updates

--- Readme.md
+++ Readme.md
@@ -1,1 +1,26 @@
 ## healpixDB
+
+Small routine to convert from RA,DEC to HEALPIX pixel indices to
+be ingested in the DB
+
+### Use
+usage: hpixDB [-h] -i INPUT -o OUTPUT -c COLUMNS [--nside NSIDE] [--hpix_col HPIX_COL]
+
+Creates Healpix indices for RA,DEC pair taken from a file
+
+optional arguments:
+  -h, --help            show this help message and exit
+
+required named arguments:
+  -i INPUT, --input INPUT
+                        The name of the input file
+  -o OUTPUT, --output OUTPUT
+                        The name of the output file
+  -c COLUMNS, --columns COLUMNS
+                        The name of the 3 columns used separated by comas,
+                        e.g.: NUMBER,RA,DEC,
+                        default=NUMBER,ALPHAWIN_J2000,DELTAWIN_J2000
+
+optional named arguments:
+  --nside NSIDE         Nside value for Healpix, has to be a power of 2
+  --hpix_col HPIX_COL   The name of the healpix indices column in the output file
--- python/hpix_tools.py
+++ python/hpix_tools.py
@@ -14,14 +14,18 @@ def main():
     names of the index, ra and dec columns and nside parameter for healpix
 
     Usage:
-    hpixDB --input <file.fits> --output <output.fits> --columns index,ra,dec --nside <number>
+    hpixDB -i <file.fits> -o <output.fits> --columns index,ra,dec
+
+    Optional:
+    --nside <number> is the nside use for healpix (12*nside*nside number of pixels)
+    --hpix_col <name> name of the output column for the indices
 
     This will generate a fits file with 2 a index column and healpix column with indices
     """
     parser = argparse.ArgumentParser(
         description="Creates Healpix indices for RA,DEC pair taken from a file",
         prog="hpixDB",
-        usage='%(prog)s [options]',
+        usage='%(prog)s [-h] -i INPUT -o OUTPUT -c COLUMNS [--nside NSIDE] [--hpix_col HPIX_COL]',
         add_help=True)
     required_args = parser.add_argument_group('required named arguments')
     required_args.add_argument("-i", "--input", type=str, action="store", default=None, required=True,
@@ -30,7 +34,8 @@ def main():
                                help="The name of the output file")
     required_args.add_argument("-c", "--columns", type=str, action="store",
                                default="NUMBER,ALPHAWIN_J2000,DELTAWIN_J2000",
-                               help="The name of the 3 columns used separated by comas, e.g.: NUMBER,RA,DEC")
+                               help="The name of the 3 columns used separated by comas, \
+                               e.g.: NUMBER,RA,DEC, default=NUMBER,ALPHAWIN_J2000,DELTAWIN_J2000")
     optional_args = parser.add_argument_group('optional named arguments')
     optional_args.add_argument("--nside", type=int, action="store", default=16384,
                                help="Nside value for Healpix, has to be a power of 2")
