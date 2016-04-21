f230adc320226a995d464f26adb9a9cf0977b596
Matias Carrasco Kind <mgckind@gmail.com>
2016-04-21 14:01:08 -0500

    added setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+import distutils
+from distutils.core import setup
+import glob
+
+bin_files = glob.glob("bin/*") 
+
+setup(name='hpixDB',
+      version ='1.0.1',
+      license = "NCSA",
+      description = "Small routine to convert from RA,DEC to HEALPIX pixel indices to
+be ingested in the DB",
+      author = "Matias Carrasco Kind",
+      author_email = "mcarras2@illinois.edu",
+      packages = ['hpixDB'],
+      package_dir = {'': 'python'},
+      scripts =  ['bin/hpixDB'],
+      data_files=[('ups',['ups/hpixDB.table']),
+                  ],
+      )
